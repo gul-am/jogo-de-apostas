96fde40d87bc8976d0d7eee843bd0f1822998a0f
gul-amd-moz <100712631+gul-amd-moz@users.noreply.github.com>
2022-03-17 18:05:43 +0200

    alteracoes finais

--- jogoDeApostas/src/jogodeapostas/JogoDeApostas.java
+++ jogoDeApostas/src/jogodeapostas/JogoDeApostas.java
@@ -12,50 +12,49 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class JogoDeApostas {
-
+    public static int y;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-            int x, y;
-            
-            do{ 
-                try{
+        // TODO code application logic here  
+        int x;
+            try{
+                do{ 
+                
                     Scanner entrada = new Scanner(System.in);
                     System.out.println("Introdusa o seu numero da sorte: ");
                     x = entrada.nextInt();
                     
                     if(x>=1 & x<=9){
-                        System.out.println("\n Obrigado");
+                        System.out.println("\n Processando...");
+                        Thread.sleep(3000);
+                        
+                        Random aleatorio = new Random();
+                        y = aleatorio.nextInt(1, 9);
+                
+                        if(x==y){
+                            System.out.println(" ");
+                            System.out.println("Parabens, voce ganhou!\n");
+                            System.out.printf("Numero do Sistema = %d \n", y);
+                            System.out.printf("O Numero inserido = %d \n", x);
+                            System.out.printf("\n", x);
+                        } else{
+                            System.out.println(" ");
+                            System.out.println("\n Voce perdeu!\n");
+                            System.out.printf("Numero do Sistema = %d \n", y);
+                            System.out.printf("O Numero inserido = %d \n", x);
+                            System.out.println("\n______________________________________\n Tente de novo \n");          
+                        }
+                         
                     }else{
                         System.out.println("O valor Inserido e invalido!");
                     }
                     
-                }catch (Exception e) {
-                    System.out.println("Inseriu um dado no formato desconheciso!!! \n");
-                }
-                
-                Random aleatorio = new Random();
-                y = aleatorio.nextInt(1, 9);
-                
-                if(x==y){
-                        System.out.println(" ");
-                        System.out.println("Parabens, voce ganhou!\n");
-                        System.out.printf("Numero do Sistema = %d \n", y);
-                        System.out.printf("O Numero inserido = %d \n", x);
-                        System.out.printf("\n", x);
-                } else{
-                        System.out.println(" ");
-                        System.out.println("\n Voce perdeu!\n");
-                        System.out.printf("Numero do Sistema = %d \n", y);
-                        System.out.printf("O Numero inserido = %d \n", x);
-                        System.out.println("\n Tente de novo \n______________________________________\n");
-                         Thread.sleep(3000);
-                                 
-                }
-            }while(x!=y);
+                }while(x!=y);
             
+            }catch (Exception e) {
+            System.out.println("Inseriu um dado no formato desconheciso!!! \n");
+       }
     }
-    
 }
